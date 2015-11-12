45a3ed1882efed468148c75b286d641e8a12dfa6
w603019289 <603019289@qq.com>
2015-11-12 17:30:04 +0800

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,7 @@ urlpatterns = patterns('',
     url(r'^up/$',up),
     url(r'^update/$',update),
     url(r'^check/$',check),
-    #(r'^hello/$',hello),
-    #(r'^haha/$',time),
-    #(r'^time/plus/(\d{1,2})/$',hours_ahead),                
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
 
     url(r'^admin/', admin.site.urls),
     
